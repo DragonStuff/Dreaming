43012c8e627b710c21ad20d5617e1ff323b7a344
DragonStuff <me@0xdragon.com>
2015-06-28 21:21:34 +1000

    Continuing Testing :floppy_disk:
    :bulb:

--- index.php
+++ index.php
@@ -106,7 +106,6 @@
       </h2>
       <h3 align="center">
         <a align="center" href="index.php" class="btn btn-primary">I'm not sure.</a>
-        <input type="hidden" value="0" id="theValue" />
         <a align="center" href="javascript:;" onclick="addElement();" class="btn btn-success">I am awake.</a>
         <div id="myDiv"> </div>
       </h3>
--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,7 @@
 
 function addElement() {
   var ni = document.getElementById('myDiv');
-  var numi = document.getElementById('theValue');
-  var num = (document.getElementById('theValue').value -1)+ 2;
-  numi.value = num;
   var newdiv = document.createElement('div');
-  var divIdName = 'my'+num+'Div';
-  newdiv.setAttribute('id',divIdName);
-  newdiv.innerHTML = '<div class="alert alert-dismissible alert-success"><button type="button" class="close" data-dismiss="alert">×</button><strong>Well done!</strong> You successfully read <a href="#" class="alert-link">this important alert message</a>.</div>';
+  newdiv.innerHTML = '<div class="alert alert-dismissible alert-success"><button type="button" class="close" data-dismiss="alert">×</button><strong>Well done!</strong> You are definitely awake</a>.</div>';
   ni.appendChild(newdiv);
 }
